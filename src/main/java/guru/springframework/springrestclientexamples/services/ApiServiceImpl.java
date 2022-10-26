a76578dd26aec30427d80a06b2c5556cfa4391bd
Monika Jakopanec <mjakopanec7@gmail.com>
2022-10-26 18:24:51 +0200

    Added UserController and API Service.

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,22 +2,29 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class ApiServiceImpl implements ApiService {
     private RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate){
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url){
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("http://private-anon-0393f2385d-apifaketory.apiary-mock.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+        UserData userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
